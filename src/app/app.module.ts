118343a54003d788af762c80a5f9db42c63d1b13
PabloRodriguezPUJ <pablo.rodriguezg@javeriana.edu.co>
2023-03-29 00:35:14 -0500

    Creación de Establecimientos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { EstablecimientosComponent } from './establecimientos/establecimientos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    QuienesSomosComponent
+    QuienesSomosComponent,
+    EstablecimientosComponent
   ],
   imports: [
     BrowserModule,
