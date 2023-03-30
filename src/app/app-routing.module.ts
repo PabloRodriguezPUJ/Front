a4ed8f1ecfb68c0f315f52f61549cf02839465ef
PabloRodriguezPUJ <pablo.rodriguezg@javeriana.edu.co>
2023-03-30 17:46:10 -0500

    Routing parcialmente completado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { EstablecimientosComponent } from './establecimientos/establecimientos.component';
+import { EditarEstablecimientoComponent } from './editar-establecimiento/editar-establecimiento.component';
+import { CanchasComponent } from './canchas/canchas.component';
+import { EditarCanchaComponent } from './editar-cancha/editar-cancha.component';
+import { ReservasComponent } from './reservas/reservas.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'quienes-somos', component: QuienesSomosComponent },
+  { path: 'establecimientos', component: EstablecimientosComponent },
+  { path: 'editar-establecimiento', component: EditarEstablecimientoComponent },
+  { path: 'canchas', component: CanchasComponent },
+  { path: 'editar-cancha', component: EditarCanchaComponent },
+  { path: 'reservas', component: ReservasComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
